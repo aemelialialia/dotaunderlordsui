87fcf6b1135d6c118f415da28eedcaa876515407
Emily Zhang <emily.aiwen@gmail.com>
2019-08-11 22:32:53 -0700

    update files

--- Assets/underlords-web-master/src/components/Icons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as IconSteam } from './IconSteam';
-export { default as IconGithub } from './IconGithub';
-export { default as IconArrowDown } from './IconArrowDown';
-export { default as IconArrowUp } from './IconArrowUp';
