0104369527dc1cf766c9856244beb8e78690ac44
Emily Zhang <emily.aiwen@gmail.com>
2019-08-19 00:02:28 -0700

    some stuff

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import './App.css';
+import {IoIosFiling} from "react-icons/io";
+import {IoIosSettings} from "react-icons/io";
+import {IoIosPower} from "react-icons/io";
+import {IoIosMenu} from "react-icons/io";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button className="protopass-button">PROTOPASS</button>
+        <button className="protopass-button">
+          <img alt= "" src="/button-texture.jpg" />
+          PROTOPASS</button>
         <button className="game-button">GAME</button>
         <button className="vault-button">VAULT</button>
       </header>
         <div className="menu">
-          <button className="menu-button"/>
+          <IoIosMenu className="menu-button"
+          style={{height: 40, width: 40}}/>
         </div>
 
         <div className="secondary-menu">
-          <button className="feedback-button"/>
-          <button className="settings-button"/>
-          <button className="exit-button"/>
+            <IoIosFiling  className="feedback-button"
+            style={{height: 40, width: 40}} />
+            <IoIosSettings className="settings-button"
+            style={{height: 40, width: 40}} />
+            <IoIosPower className="exit-button"
+             style={{height: 40, width: 40}} />
         </div>
     </div>
   );
